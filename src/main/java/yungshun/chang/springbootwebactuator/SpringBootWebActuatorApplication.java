24026773a8f620bc5d344315eb4d759c3be69423
yungshun317 <seraph317@gmail.com>
2018-07-22 18:11:40 +0800

    Enable the Actuator endpoints by assigning their IDs to management.endpoints.web.exposure.include in application.properties

--- src/main/java/yungshun/chang/springbootwebactuator/SpringBootWebActuatorApplication.java
+++ src/main/java/yungshun/chang/springbootwebactuator/SpringBootWebActuatorApplication.java
@@ -3,10 +3,18 @@ package yungshun.chang.springbootwebactuator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 @SpringBootApplication
 public class SpringBootWebActuatorApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringBootWebActuatorApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String index(){
+        return "Spring Boot Actuator";
     }
 }
