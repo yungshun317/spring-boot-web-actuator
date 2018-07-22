1e64efe77ff7d9ce1ba203443462d8093cd5326b
yungshun317 <seraph317@gmail.com>
2018-07-22 18:05:42 +0800

    Create a Spring Boot project using Spring Initializr via IntelliJ

---
+++ src/main/java/yungshun/chang/springbootwebactuator/SpringBootWebActuatorApplication.java
@@ -0,0 +1,12 @@
+package yungshun.chang.springbootwebactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebActuatorApplication.class, args);
+    }
+}
